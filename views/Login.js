03f90a8a9c8bba92e40719c819b1fbc20a64e0d7
narayanvaishali <narayanvaishali@gmail.com>
2019-07-25 13:37:41 +0100

    login - amended

--- views/Login.js
+++ views/Login.js
@@ -10,67 +10,7 @@ import { BrowserRouter as Router, Route,Switch, Redirect} from 'react-router-dom
 import { AuthConsumer } from '../Components/AuthContext';
 import Layout from '../Layout';
 import styles from './styles';
-/*
-const Login = () => (
-<AuthConsumer>
-{({ isAuth, login, logout }) => (
-      <Layout drawer="false">
-        <div className="container">
-          <Grid container justify="center">
-            <Grid item xs={12} sm={8} md={4}>
-              <form
-                className={classes.loginForm}
-                noValidate
-                autoComplete="off"
-                onSubmit={login}
-              >
-                <h2>Login</h2>
-                {message && (
-                  <SnackbarContent
-                    className={classes.errorMessage}
-                    aria-describedby="login-error"
-                    message={(
-                      <span id="login-error" className={classes.message}>
-                        <ErrorIcon className={classes.messageIcon} />
-                        {message}
-                      </span>
-                    )}
-                  />
-                ) }
-                <TextField
-                  id="outlined-email-input"
-                  label="Email"
-                  type="email"
-                  name="email"
-                  autoComplete="email"
-                  margin="normal"
-                  fullWidth
-                  value={email}
-                  onChange={this.handleChange}
-                />
-                <TextField
-                  id="outlined-passowrd-input"
-                  label="Password"
-                  type="password"
-                  name="password"
-                  autoComplete="password"
-                  margin="normal"
-                  fullWidth
-                  value={password}
-                  onChange={this.handleChange}
-                />
-                <Button variant="outlined" color="secondary" className={classes.button} type="submit">
-                  Login
-                </Button>
-              </form>
-            </Grid>
-          </Grid>
-        </div>
-      </Layout>
-   )}
-</AuthConsumer>
-)
-*/
+
 
 class Login extends Component  {
 render () {
